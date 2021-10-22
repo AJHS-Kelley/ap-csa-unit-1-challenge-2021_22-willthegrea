53f5d3a50d6bce3e39c733aafdd87c9455891988
Mauricio Salmoran <70659429+s11653565@users.noreply.github.com>
2021-10-22 08:32:51 -0400

    Update DamageDealt.java
    peer bugging by mauricio salmoran

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,5 +1,5 @@
 // <Byson William>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10-20-21>, <8:36>, v0.0
-
+// peer bugging by mauricio salmoran
 import java.until.scanner
 
 public class DamageDelt[
